fb1802a82f80fc50a135451117a81bdf8e569920
Joel Niklaus <me@joelniklaus.ch>
2020-09-23 09:54:27 +0200

    relaxed install requirements

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import setuptools
 
 setuptools.setup(
     name='loss_landscape',
-    version='0.0.6.dev3',
-    install_requires=['numpy==1.18.4', 'h5py==2.10.0', 'matplotlib==3.2.1', 'scipy==1.4.1',
-                      'scikit-learn==0.22.2.post1', 'seaborn==0.10.1', 'mpi4py==3.0.3'],  # TODO relax this
+    version='0.0.6.dev4',
+    install_requires=['mpi4py>=3.0.3', 'h5py>=2.10.0', 'numpy>=1.18.4', 'scipy>=1.4.1',
+                      'scikit-learn>=0.22.2.post1', 'matplotlib>=3.2.1', 'seaborn>=0.10.1'],  # TODO relax this
     python_requires='>=3',
     author="Joel Niklaus",
     author_email="me@joelniklaus.ch",
