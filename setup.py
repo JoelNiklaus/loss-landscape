b6ed2e2d6aa4a20568f1fcb196963bfbaad3bd68
Joel Niklaus <me@joelniklaus.ch>
2020-09-23 10:02:00 +0200

    removed mpi4py because if installed with pip it can cause problems (should be installed with conda)

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import setuptools
 
 setuptools.setup(
     name='loss_landscape',
-    version='0.0.6.dev4',
-    install_requires=['mpi4py>=3.0.3', 'h5py>=2.10.0', 'numpy>=1.18.4', 'scipy>=1.4.1',
-                      'scikit-learn>=0.22.2.post1', 'matplotlib>=3.2.1', 'seaborn>=0.10.1'],  # TODO relax this
+    version='0.0.6.dev5',
+    install_requires=['h5py>=2.10.0', 'numpy>=1.18.4', 'scipy>=1.4.1',
+                      'scikit-learn>=0.22.2.post1', 'matplotlib>=3.2.1', 'seaborn>=0.10.1'],
     python_requires='>=3',
     author="Joel Niklaus",
     author_email="me@joelniklaus.ch",
